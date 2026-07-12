065c9aab3b7fe1c532042bfa6855a80a626737ef
NewLegend01 <newaera01@gmail.com>
2026-07-13 02:48:24 +0700

    GM13-7-69 upload

---
+++ offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1783885543,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg_instructions-sheet0.png",
+		"images/btn_start-sheet0.png",
+		"images/btn_instructions-sheet0.png",
+		"images/btn_back-sheet0.png",
+		"images/bg_menu-sheet0.png",
+		"images/bg_gameplay-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/bg_menu2-sheet0.png",
+		"images/bg_gameplay_minus-sheet0.png",
+		"images/bg_gameplay_multiplied-sheet0.png",
+		"images/bg_gameplay_divide-sheet0.png",
+		"images/bg_gameover-sheet0.png",
+		"images/life.png",
+		"images/sprite4-sheet0.png",
+		"images/bullet-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
